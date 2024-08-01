1ee6f7acdc7f21c47d60f9cf2616da01c52bb43b
AnnaGoot <a.gutikova00@gmail.com>
2024-08-01 20:57:29 +0200

    Builds Added

---
+++ Assets/Scripts/Editor/CompileFactory.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Compiles
+{
+    public class CompileFactory : MonoBehaviour
+    {
+        [MenuItem("CompileFactory/Compile")]
+        public static void Compile()
+        {
+            new ManyCompileFactory(
+                new ExtentionFolderCompileFactory(
+                    new SimpleCompileFactory(BuildTarget.StandaloneWindows64),
+                    ".exe"
+                    ),
+                new ExtentionFolderCompileFactory(
+                    new APKCompileFactory(),
+                    ".apk"
+                    )
+                    ).Compile(
+                    EditorUtility.OpenFolderPanel(
+                        "Choose Folder", "Assets", "Build"
+                        ), BuildOptions()
+                );
+        }
+
+        public static BuildOptions BuildOptions()
+        {
+            return GetBuildPlayerOptions().options;
+        }
+
+        private static BuildPlayerOptions GetBuildPlayerOptions(BuildPlayerOptions defaultOptions = new())
+        {
+            return BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(defaultOptions);
+        }
+
+    }
+}
